593131df34bf80f66f34b66e9b1dba076b5bde45
Matheus Henrique <matheus.mhsantos@mercadolivre.com>
2024-02-02 00:38:47 -0300

    feat: add initial booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,11 +3,13 @@ import { format } from "date-fns";
 import Header from "@/components/header";
 import { ptBR } from "date-fns/locale";
 import Search from "./components/search";
+import BookingItem from "@/components/booking-item";
 
 export default function Home() {
   return (
     <main>
       <Header />
+
       <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá, Matheus!</h2>
           <p className="capitalize text-sm">
@@ -19,6 +21,13 @@ export default function Home() {
 
       <div className="px-5 mt-6">
         <Search />
+      </div>
+
+      <div className="px-5 mt-6">
+        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">
+          Agendamentos
+        </h2>
+        <BookingItem />
       </div>
     </main>
   );
