20d5b1b3e0ad326cd3461cd5eb6c187f783ee4b5
Matheus Henrique <matheus.mhsantos@mercadolivre.com>
2024-02-02 01:54:56 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -35,8 +35,20 @@ export default async function Home() {
       </div>
 
       <div className="mt-6">
-        <h2 className="px-5 text-xs uppercase text-gray-400 font-bold">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
           Recomendados
+        </h2>
+          
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-6 mb-10 pb-16">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Populares
         </h2>
           
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div className="bottom-0 left-0 w-full bg-secondary py-6 px-5">
+            <p className="text-gray-400 text-xs font-bold opacity-75">&copy; 2024 Copyright Matheus Henrique Santos da Silva</p>
+        </div>
+     );
+}
+ 
+export default Footer;
